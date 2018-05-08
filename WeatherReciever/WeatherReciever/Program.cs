82f746685ca74b8a3c2b1eaa1d1fef33f2bedaf8
NikoDyring <Nikodyring@gmail.com>
2018-05-08 12:15:42 +0200

    Now posts into the database.

--- WeatherReciever/WeatherReciever/Program.cs
+++ WeatherReciever/WeatherReciever/Program.cs
@@ -1,56 +1,84 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Data.SqlClient;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace WeatherReciever
 {
     /// <summary>
-    /// Modtager Data fra Pi
+    ///     Modtager Data fra Pi
     /// </summary>
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public int QQ = 0;
+
+        public static void Main(string[] args)
         {
-            UdpClient udpServer = new UdpClient(9877);
-            string _recievedData;
+            var pm = new Program();
+            var udpServer = new UdpClient(9877);
 
-        //Creates an IPEndPoint to record the IP Address and port number of the sender.  
-        IPAddress ip = IPAddress.Parse("192.168.6.145");
-            IPEndPoint RemoteIpEndPoint = new IPEndPoint(ip, 9877);
+            //Creates an IPEndPoint to record the IP Address and port number of the sender.  
+            var ip = IPAddress.Parse("192.168.6.145");
+            var RemoteIpEndPoint = new IPEndPoint(ip, 9877);
 
+
+            
             try
             {
                 // Blocks until a message is received on this socket from a remote host (a client).
                 Console.WriteLine("Server is Online");
+
                 while (true)
                 {
-                    Byte[] receiveBytes = udpServer.Receive(ref RemoteIpEndPoint);
+                    var receiveBytes = udpServer.Receive(ref RemoteIpEndPoint);
                     //Server is now activated");
 
-                    string receivedData = Encoding.ASCII.GetString(receiveBytes);
-                    _recievedData = receivedData;
+                    var receivedData = Encoding.ASCII.GetString(receiveBytes);
 
                     Console.WriteLine(receivedData);
                     //Console.WriteLine("Received from: " + clientName.ToString() + " " + text.ToString());
 
-                
-                  
+                    pm.ToDataBase(receivedData);
+
+                    if (pm.QQ > 3)
+                    {
+                        break;
+                    }
                 }
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.ToString());
             }
-        
-    }
+        }
 
         public void ToDataBase(string temp)
         {
-            
+            var time = DateTime.Now.ToShortTimeString();
+            var place = "Roskilde";
+
+            var _connectionString = "Server=tcp:3semesterxxx.database.windows.net,1433;Initial Catalog=WCFSTUDENT;Persist Security Info=False;User ID=Admeme;Password=Skole123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                var sqlQuery =
+                    "INSERT INTO Temperature (Temperature, Time, Place) VALUES (@Temperature, @Time, @Place)";
+
+                using (var command = new SqlCommand(sqlQuery, connection))
+                {
+                    command.Parameters.AddWithValue("@Temperature", temp);
+                    command.Parameters.AddWithValue("@Time", time);
+                    command.Parameters.AddWithValue("@Place", place);
+
+                    connection.Open();
+                    var result = command.ExecuteNonQuery();
+
+                    if (result < 0) Console.WriteLine("Error inserting data into the database.");
+                    QQ++;
+                }
+            }
         }
     }
 }
